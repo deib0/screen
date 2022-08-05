be14b48d82a6129ed7b40874fa9a716e4dae6055
deibo <deib0@foxmail.com>
2022-08-05 15:41:05 +0800

    使用flex和grid完成基本布局

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -5,6 +5,13 @@ export const Home = () => {
   return (
     <div className='home'>
       <div className="header"></div>
+      <main className='main'>
+        <section className="section1"></section>
+        <section className="section2"></section>
+        <section className="section3"></section>
+        <section className="section4"></section>
+        <section className="section5"></section>
+      </main>
     </div>
   );
 };
