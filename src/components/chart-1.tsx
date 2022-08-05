49c94b76ee5feab63f4f52a821e9e9835aacfe8d
deibo <deib0@foxmail.com>
2022-08-05 19:40:52 +0800

    将chart1封装为独立组件

---
+++ src/components/chart-1.tsx
@@ -0,0 +1,63 @@
+import React, {useEffect, useRef} from 'react';
+import * as echarts from 'echarts';
+export const Chart1 = () => {
+  const px = (n) => n / 2420 * (window as any).pageWidth;
+  const divRef = useRef(null);
+  useEffect(() => {
+    var myChart = echarts.init(divRef.current);
+    myChart.setOption({
+      textStyle: {
+        fontSize: px(12),
+        color: '#79839E'
+      },
+      title: {show: false},
+      legend: {show: false},
+      xAxis: {
+        data: ['城关区', '七里河区', '西固区', '安宁区', '红谷区', '永登区', '臬兰区', '榆中区', '兰州新区'],
+        axisTick: {show: false},
+        axisLine: {
+          lineStyle: {color: '#083B70'}
+        },
+        axisLabel: {
+          fontSize: px(12),
+          formatter(val) {
+            if (val.length > 2) {
+              const array = val.split('');
+              array.splice(2, 0, '\n');
+              return array.join('');
+            } else {
+              return val;
+            }
+          }
+        },
+      },
+      grid: {
+        x: px(40),
+        y: px(20),
+        x2: px(40),
+        y2: px(70),
+      },
+      yAxis: {
+        splitLine: {show: false},
+        axisLine: {
+          show: true,
+          lineStyle: {color: '#083B70'}
+        },
+        axisLabel: {
+          fontSize: px(12)
+        }
+      },
+      series: [{
+        type: 'bar',
+        data: [10, 20, 36, 41, 15, 26, 37, 18, 29]
+      }]
+    });
+  }, []);
+  return (
+          <div className="bordered chart1">
+            <h2>案发派出所管辖统计</h2>
+            <div ref={divRef} className="chart">
+            </div>
+          </div>
+  );
+};
